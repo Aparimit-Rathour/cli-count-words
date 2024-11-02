34c695fcdfe20a423ccaaca288f61df37c7b6dd0
Aparimit <aparimit2003@gmail.com>
2024-11-02 13:09:33 +0530

    commit 2

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-import commander from "commander"
+const { program } = require("commander");
 
-console.log("Succesful Init")
+program
+	.name("count-words")
+	.description("CLI for file actions")
+	.version("1.0.0")
+
+program
+	.command("count")
+	.description("Counts the number of words in a file")
+
+program
+	.argument("<string>", "string to split")
+	.option("--first", "display just the first substring")
+	.option("-s, --separator <char>", "separator character", ",")
+	.action((str, options) => {
+		const limit = options.first ? 1 : undefined;
+		console.log(str.split(options.separator, limit));
+	});
+
+program.parse();
